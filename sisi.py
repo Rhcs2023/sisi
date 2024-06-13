e0c60575f4276ba6dc106ae00173a67fc4e660e0
Space <143919787+Rhcs2023@users.noreply.github.com>
2024-06-12 22:41:02 -0600

    sisi.py

--- sisi.py
+++ sisi.py
@@ -150,7 +150,7 @@ if st.button("Traducir"):
     # Cargar la imagen desde un archivo local
   #  imagen = "http://exploraoaxaca.mx/wp-content/uploads/2018/02/Carnaval-de-Chalcatongo-Oaxaca-3.jpg"
   #  st.image(imagen, caption='Descripción de la imagen', use_column_width=True)
-imagen = "http://exploraoaxaca.mx/wp-content/uploads/2018/02/Carnaval-de-Chalcatongo-Oaxaca-3.jpg"
+imagen = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chalcatzingo.JPG/1280px-Chalcatzingo.JPG"
 st.image(imagen, caption='Descripción de la imagen', use_column_width=True)
 
 
