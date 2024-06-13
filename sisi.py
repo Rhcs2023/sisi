76b177057ed64e25f7248e13b1ce878b3151d80a
Space <143919787+Rhcs2023@users.noreply.github.com>
2024-06-12 18:10:49 -0600

    sisi.py

--- sisi.py
+++ sisi.py
@@ -135,7 +135,7 @@ def traducir_oracion(oracion):
 st.title("Traductor de español a Mixteco")
 
 # Preguntar al usuario por una oración en español
-oracion = st.text_input("Ingresa una oración en español:").lower
+oracion = st.text_input("Ingresa una oración en español:").lower()
 
 if st.button("Traducir"):
     oracion_traducida = traducir_oracion(oracion)
